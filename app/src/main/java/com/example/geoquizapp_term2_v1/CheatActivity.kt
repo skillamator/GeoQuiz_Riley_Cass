946e471853ed1a3d97e1095c536df025a32b548b
skillamator <101493025+skillamator@users.noreply.github.com>
2024-08-21 18:56:27 +1000

    cc

--- app/src/main/java/com/example/geoquizapp_term2_v1/CheatActivity.kt
+++ app/src/main/java/com/example/geoquizapp_term2_v1/CheatActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.geoquizapp_term2_v1.databinding.ActivityCheatBinding
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cheat)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
